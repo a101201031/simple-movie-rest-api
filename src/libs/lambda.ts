c232fba0f1b1e4df3a83ecf91d34abde6faa145b
a101201031 <a101201031@gmail.com>
2023-04-14 18:35:47 +0900

    feat: add error handler

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,6 +1,6 @@
+import { errorHandler } from '@middleware/errorHandler';
 import middy from '@middy/core';
 import middyCors from '@middy/http-cors';
-import middyErrorHandler from '@middy/http-error-handler';
 import middyJsonBodyParser from '@middy/http-json-body-parser';
 import middyValidator from '@middy/validator';
 import { transpileSchema } from '@middy/validator/transpile';
@@ -11,15 +11,14 @@ interface MiddfyParams {
   eventSchema?: object;
 }
 
-export const middyfy = ({ handler, eventSchema }: MiddfyParams) => {
-  const middyfyFunction = middy(handler)
-    .use(middyCors())
-    .use(middyJsonBodyParser())
-    .use(middyErrorHandler());
-
-  return eventSchema
-    ? middyfyFunction.use(
-        middyValidator({ eventSchema: transpileSchema(eventSchema) }),
-      )
-    : middyfyFunction;
-};
+export const middyfy = ({ handler, eventSchema }: MiddfyParams) =>
+  eventSchema
+    ? middy(handler)
+        .use(middyCors())
+        .use(middyJsonBodyParser())
+        .use(middyValidator({ eventSchema: transpileSchema(eventSchema) }))
+        .use(errorHandler())
+    : middy(handler)
+        .use(middyCors())
+        .use(middyJsonBodyParser())
+        .use(errorHandler());
---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,67 @@
+import type middy from '@middy/core';
+import { normalizeHttpResponse } from '@middy/util';
+import type createHttpError from 'http-errors';
+
+interface ApiErrorTypes extends Partial<createHttpError.HttpError> {
+  code: string;
+  headers?: any;
+}
+
+interface OptionTypes {
+  logger?: ((error: any) => void) | boolean;
+  fallbackCode?: string;
+  fallbackMessage?: string;
+}
+
+const defaults: OptionTypes = {
+  logger: console.error,
+  fallbackCode: 'internal_server_error',
+  fallbackMessage: 'internal server error',
+};
+
+export const errorHandler = (opts: OptionTypes = {}): middy.MiddlewareObj => {
+  const options = {
+    ...defaults,
+    ...opts,
+  };
+  const errorHandlerOnError: middy.MiddlewareFn = async (request) => {
+    let error = request.error as unknown as ApiErrorTypes;
+    if (request.response !== undefined) return;
+    if (typeof options.logger === 'function') {
+      options.logger(error);
+    }
+    if (error.statusCode && error.expose === undefined) {
+      error.expose = error.statusCode < 500;
+    }
+    if (options.fallbackCode && (!error.statusCode || !error.expose)) {
+      error = {
+        statusCode: 500,
+        code: options.fallbackCode,
+        expose: true,
+      };
+    }
+    if (error.expose) {
+      normalizeHttpResponse(request);
+      const { statusCode, code, message, headers } = error;
+      request.response = {
+        ...request.response,
+        statusCode,
+        body: JSON.stringify({
+          error: {
+            code,
+            message,
+          },
+          success: false,
+        }),
+        headers: {
+          ...headers,
+          ...request.response.headers,
+          'Content-Type': 'application/json',
+        },
+      };
+    }
+  };
+  return {
+    onError: errorHandlerOnError,
+  };
+};
