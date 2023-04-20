5654f2b88126571c59c8535cc8ee26708b2037d9
a101201031 <a101201031@gmail.com>
2023-04-20 17:50:22 +0900

    chore: remove unused api

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,18 +1,6 @@
 import { handlerPath } from '@libs/handler-resolver';
 
 export default {
-  hello: {
-    handler: `${handlerPath(__dirname)}/functions/hello.hello`,
-    events: [
-      {
-        http: {
-          method: 'get',
-          path: 'hello',
-          cors: true,
-        },
-      },
-    ],
-  },
   init: {
     handler: `${handlerPath(__dirname)}/functions/init.init`,
     events: [
